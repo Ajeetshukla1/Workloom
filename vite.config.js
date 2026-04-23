a03066fdc87de2d4dcadc92370c62a927cf3e127
Ajeetshukla1 <ajeetshukla543@gmail.com>
2026-04-24 02:05:59 +0530

    pipeline created

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Determine the base URL dynamically based on GitHub Actions environment
+const repoName = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: repoName,
 })
